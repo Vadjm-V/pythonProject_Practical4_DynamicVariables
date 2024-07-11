156ef458cec2bc31917019b5079d9e333abc6998
Vadjm-V <Vadjm00@gmail.com>
2024-07-11 15:57:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+name = 'Вадим'
+print('Мое имя', name)
+age = 50
+print('Мой возраст', age, 'лет')
+age += 5
+print('Мой возраст стал', age, 'лет')
+is_student = True
+print('Is Student -',is_student)
